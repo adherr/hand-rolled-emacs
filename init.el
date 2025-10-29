60e8f7200d54ca978b45495b730702ef60cf71db
Andrew Herr <andrew.herr@testdouble.com>
2025-10-29 13:17:48 -0600

    re-copy native comp setup

--- init.el
+++ init.el
@@ -27,29 +27,27 @@
 (use-package exec-path-from-shell
   :config
   (exec-path-from-shell-initialize)
+  ;; native comp stolen from https://github.com/xenodium/dotsies/blob/main/emacs/features/fe-package-extensions.el#L19
   (if (and (fboundp 'native-comp-available-p)
 	   (native-comp-available-p))
       (progn
 	(message "Native comp is available")
 	(when (eq system-type 'darwin)
-	  (progn
-	    ;; bin inside the Emacs.app
-	    (add-to-list 'exec-path (concat invocation-directory "bin") t)
-	    ;; this is a combination of https://xenodium.com/trying-out-gccemacs-on-macos/
-	    ;; and stuff from this thread https://github.com/d12frosted/homebrew-emacs-plus/issues/378#issuecomment-1666548762
-	    (setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
-					   (when (getenv "LIBRARY_PATH")
-					     ":")
-					   (mapconcat (lambda (path) (car (file-expand-wildcards path)))
-						      '("/usr/local/opt/gcc/lib/gcc/*"
-							"/usr/local/opt/libgccjit/lib/gcc/*"
-							"/usr/local/opt/gcc/lib/gcc/*/gcc/*/*"
-							"/opt/homebrew/opt/gcc/lib/gcc/*"
-							"/opt/homebrew/opt/libgccjit/lib/gcc/*"
-							"/opt/homebrew/opt/gcc/lib/gcc/*/gcc/*/*")
-						      ":")))))
+	  (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
+	(setq native-comp-async-report-warnings-errors 'silent)
+	;; Using Emacs.app/Contents/MacOS/bin since it was compiled with
+	;; ./configure --prefix="$PWD/nextstep/Emacs.app/Contents/MacOS"
+	;; Append to path to give priority to values from exec-path-from-shell-initialize.
+	(add-to-list 'exec-path (concat invocation-directory (file-name-as-directory "bin")) t)
+	(setenv "LIBRARY_PATH" (concat (getenv "LIBRARY_PATH")
+				       (when (getenv "LIBRARY_PATH")
+					 ":")
+				       ;; This is where Homebrew puts libgccjit libraries.
+				       (car (file-expand-wildcards
+					     (expand-file-name "/opt/homebrew/opt/libgccjit/lib/gcc/*")))))
 	;; Only set after LIBRARY_PATH can find gcc libraries.
-	(setq comp-deferred-compilation t))
+	(setq comp-deferred-compilation t)
+	(setq comp-speed 3))
     (message "Native comp is *not* available"))
   (dolist (var '("LANG" "LC_CTYPE" "LIBRARY_PATH" "LSP_USE_PLISTS" "SSH_AUTH_SOCK"))
     (add-to-list 'exec-path-from-shell-variables var)))
